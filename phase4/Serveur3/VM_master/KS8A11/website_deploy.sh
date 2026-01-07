4a4d4b56591262f7c247944b0712f133059b34de
celian0 <cemalilian@gmail.com>
2026-01-07 15:30:30 +0100

    correction manifest web + ajout script deploiement site web

---
+++ phase4/Serveur3/VM_master/KS8A11/website_deploy.sh
@@ -0,0 +1,7 @@
+cd /var/www/html
+wget https://github.com/archidote/get-ready-simple-countdown-html-css-js/archive/refs/heads/main.zip
+echo Téléchargement commencé
+unzip main.zip
+echo Extraction des fichiers
+rm main.zip
+echo Suppression de l'archive
